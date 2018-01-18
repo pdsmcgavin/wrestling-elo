3d3cb0d39c38a8e29fbcd2831dd96b61a1aca2b7
McGavInvincible <pdsmcgavin@gmail.com>
2018-01-18 10:05:56 +0000

    Started some pattern matching

--- lib/wwelo_api/site_scraper/site_urls.ex
+++ lib/wwelo_api/site_scraper/site_urls.ex
@@ -1,6 +1,6 @@
 defmodule WweloApi.SiteScraper.SiteUrls do
 
-  def wwe_events__results_url(year, page_number) do
+  def wwe_events_results_url(%{year: year, page_number: page_number}) do
     results_number = (page_number - 1)*100
 
     "https://www.cagematch.net/?id=1&view=search&sPromotion=1&sDateFromDay=01&sDateFromMonth=01&sDateFromYear=#{year}&sDateTillDay=31&sDateTillMonth=12&sDateTillYear=#{year}&sEventType=TV-Show%7CPay+Per+View&s=#{results_number}"
