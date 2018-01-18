50f589bc2aa5379951abb6ffb82fbcbe1a8c27f0
McGavInvincible <pdsmcgavin@gmail.com>
2018-01-18 16:57:24 +0000

    List of event urls returned for year and page number, need to improve readability

--- lib/wwelo_api/site_scraper/site_urls.ex
+++ lib/wwelo_api/site_scraper/site_urls.ex
@@ -6,4 +6,24 @@ defmodule WweloApi.SiteScraper.SiteUrls do
     "https://www.cagematch.net/?id=1&view=search&sPromotion=1&sDateFromDay=01&sDateFromMonth=01&sDateFromYear=#{year}&sDateTillDay=31&sDateTillMonth=12&sDateTillYear=#{year}&sEventType=TV-Show%7CPay+Per+View&s=#{results_number}"
   end
 
+  def wwe_event_urls_list(params = %{year: _, page_number: _}) do
+
+    response = HTTPoison.get!(wwe_events_results_url(params))
+
+     [_ | event_rows] = response.body
+     |> Floki.find(".TableContents")
+     |> Enum.at(0)
+     |> elem(2)
+     |> Enum.at(0)
+     |> elem(2)
+
+    event_rows
+    |> Enum.map(fn(x) -> x
+    |> elem(2)
+    |> Enum.at(2)
+    |> elem(2)
+    |> Enum.at(2) end)
+
+  end
+
 end
--- mix.exs
+++ mix.exs
@@ -38,7 +38,9 @@ defmodule WweloApi.Mixfile do
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:httpoison, "~> 1.0"},
+      {:floki, "~> 0.18.0"}
     ]
   end
 
