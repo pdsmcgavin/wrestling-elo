62fa31d1ae7d198b2f9ed800c63c8141352f2739
McGavInvincible <pdsmcgavin@gmail.com>
2018-01-19 17:31:29 +0000

    Transforming event data to be saved to event table

--- lib/wwelo_api/site_scraper/data_scraper.ex
+++ lib/wwelo_api/site_scraper/data_scraper.ex
@@ -4,6 +4,42 @@ defmodule WweloApi.SiteScraper.DataScraper do
     response = HTTPoison.get!("https://www.cagematch.net/"<>event_url_path)
 
     response.body
+
+    [{_, _, event_information}] = response.body |> Floki.find(".InformationBoxTable")
+    # event_matches = response.body |> Floki.find(".Matches")
+
+    event_information |> map_event_info
+  end
+
+  def map_event_info(event_info) do
+
+    Enum.reduce(event_info, %{}, fn(x, acc) ->
+      IO.inspect x
+      case x do
+        {_, _, [{_, _, ["Name of the event:"]}, {_, _, [event_name]}]} -> Map.put(acc, :name, event_name)
+        {_, _, [{_, _, ["Promotion:"]}, {_, _, [{_, _, [promotion]}]}]} -> Map.put(acc, :promotion, promotion)
+        {_, _, [{_, _, ["Type:"]}, {_, _, [event_type]}]} -> Map.put(acc, :event_type, event_type)
+        {_, _, [{_, _, ["Location:"]}, {_, _, [location]}]} -> Map.put(acc, :location, location)
+        {_, _, [{_, _, ["Arena:"]}, {_, _, [arena]}]} -> Map.put(acc, :arena, arena)
+        {_, _, [{_, _, ["Date:"]}, {_, _, [date]}]} -> case format_date(date) do
+                                                        {:ok, date} -> Map.put_new(acc, :date, date)
+                                                        _ -> acc
+                                                       end
+        {_, _, [{_, _, ["Broadcast date:"]}, {_, _, [date]}]} -> case format_date(date) do
+                                                                  {:ok, date} -> Map.put(acc, :date, date)
+                                                                  _ -> acc
+                                                                 end
+        _ -> acc
+      end
+    end)
+
+  end
+
+  def format_date(date) do
+
+    [day, month, year] = String.split(date, ".")
+
+    Ecto.Date.cast({year, month, day})
   end
 
 end
