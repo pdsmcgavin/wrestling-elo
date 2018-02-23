73e584f92bba2d4efc122e31ef73cb1e97af71f7
McGavInvincible <pdsmcgavin@gmail.com>
2018-02-23 15:15:59 +0000

    Updated test for preferred output of match results

--- test/wwelo_api/site_scraper/participants_test.exs
+++ test/wwelo_api/site_scraper/participants_test.exs
@@ -12,13 +12,30 @@ defmodule WweloApi.SiteScraper.ParticipantsTest do
       ]
     }
 
-    @singles_outcome %{winners: "AJ Styles", losers: "Baron Corbin"}
+    @singles_outcome MapSet.new([
+                       %{
+                         alias: "AJ Styles",
+                         profile_url: "?id=2&nr=801",
+                         outcome: "win",
+                         match_team: 0
+                       },
+                       %{
+                         alias: "Baron Corbin",
+                         profile_url: "12474",
+                         outcome: "loss",
+                         match_team: 1
+                       }
+                     ])
 
     test "Singles match with wrestler's with profiles" do
-      assert @singles_outcome =
-               Participants.split_result_into_winners_and_losers(
-                 @singles_match_result
-               )
+      map_set_output =
+        MapSet.new(
+          Participants.split_result_into_winners_and_losers(
+            @singles_match_result
+          )
+        )
+
+      assert @singles_outcome = map_set_output
     end
   end
 end
