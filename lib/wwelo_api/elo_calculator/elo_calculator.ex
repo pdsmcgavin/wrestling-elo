c5e0432beaeba538d6042acbbc233b57bbcde34c
McGavInvincible <pdsmcgavin@gmail.com>
2018-03-02 17:43:09 +0000

    Ecto query work on finding earliest matchs to be calculated for elo change

--- lib/wwelo_api/elo_calculator/elo_calculator.ex
+++ lib/wwelo_api/elo_calculator/elo_calculator.ex
@@ -1,4 +1,13 @@
 defmodule WweloApi.EloCalculator.EloCalculator do
+  import Ecto.Query
+
+  alias WweloApi.Repo
+  alias WweloApi.Stats.Alias
+  alias WweloApi.Stats.Event
+  alias WweloApi.Stats.Match
+  alias WweloApi.Stats.Participant
+  alias WweloApi.Stats.Wrestler
+
   @match_weight 32
   @elo_base 10
   @distribution_factor 400
@@ -25,5 +34,37 @@ defmodule WweloApi.EloCalculator.EloCalculator do
     |> Enum.map(fn {elos, change} ->
       Enum.map(elos, &(&1 + change / Enum.count(elos)))
     end)
+  end
+
+  # TODO. Separate into smaller functions
+  def event_match_participant_join(amount) do
+    query =
+      from(
+        e in Event,
+        join: m in Match,
+        where: m.event_id == e.id,
+        join: p in Participant,
+        where: p.match_id == m.id,
+        join: a in Alias,
+        where: a.id == p.alias_id,
+        join: w in Wrestler,
+        where: w.id == a.wrestler_id
+      )
+
+    query =
+      from(
+        [e, m, p, a, w] in query,
+        select:
+          {e.name, e.date, m.card_position, p.outcome, a.name, w.current_elo},
+        order_by: [
+          asc: e.date,
+          asc: e.id,
+          asc: m.card_position,
+          desc: p.outcome
+        ],
+        where: p.elo_after |> is_nil
+      )
+
+    Repo.all(query) |> Enum.slice(0, amount)
   end
 end
