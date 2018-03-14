b621241f670520e365839aeedc9de052b88ff51a
McGavInvincible <pdsmcgavin@gmail.com>
2018-03-14 23:20:14 +0000

    Added /id to stats endpoint for single wrestler elo and date information

--- lib/wwelo_api_web/controllers/stats_controller.ex
+++ lib/wwelo_api_web/controllers/stats_controller.ex
@@ -42,7 +42,34 @@ defmodule WweloApiWeb.StatsController do
   end
 
   def show(conn, %{"id" => id}) do
-    elos = [%{id: id}]
+    query =
+      from(
+        elos in Elo,
+        join: m in Match,
+        on: m.id == elos.match_id,
+        join: e in Event,
+        on: e.id == m.event_id,
+        join: w in Wrestler,
+        on: w.id == elos.wrestler_id
+      )
+
+    query =
+      from(
+        [elos, m, e, w] in query,
+        select: %{name: w.name, date: e.date, elo: elos.elo},
+        order_by: [
+          asc: w.id,
+          asc: e.date,
+          asc: e.id,
+          asc: m.card_position
+        ],
+        where: w.id == ^id
+      )
+
+    elos =
+      Repo.all(query)
+      |> Enum.group_by(&Map.get(&1, :name), &Map.delete(&1, :name))
+
     json(conn, elos)
   end
 end
