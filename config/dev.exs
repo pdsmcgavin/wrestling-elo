5e071dc95eaa70953daa6986b162f8c90ef91b62
McGavInvincible <pdsmcgavin@gmail.com>
2018-04-13 16:23:27 +0100

    Replaced brunch with webpack, time will tell if this was worth it

--- config/dev.exs
+++ config/dev.exs
@@ -13,9 +13,9 @@ config :wwelo, WweloWeb.Endpoint,
   check_origin: false,
   watchers: [
     node: [
-      "node_modules/brunch/bin/brunch",
-      "watch",
-      "--stdin",
+      "node_modules/.bin/webpack-dev-server", "--inline", "--colors", "--hot",
+      "--stdin", "--host", "localhost", "--port", "8080", "--public",
+      "localhost:8080",
       cd: Path.expand("../assets", __DIR__)
     ]
   ]
