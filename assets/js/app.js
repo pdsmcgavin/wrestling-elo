5fc4308cfe1ffe137d7246f3f8b337ecb78375c5
McGavInvincible <pdsmcgavin@gmail.com>
2018-05-21 16:25:27 +0100

    Fixed css console complaints

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 import "phoenix_html";
 import "react-phoenix";
 import WrestlerEloTable from "./components/wrestlerEloTable";
+import "../stylus/app.styl";
 
 window.Components = {
   WrestlerEloTable
