b04b5139fbf40f7a6a7da1d91191b2f29ce731f6
McGavInvincible <pdsmcgavin@gmail.com>
2018-06-06 16:48:45 +0100

    Added new query for wrestlerEloTable

--- lib/wwelo_web/schema.ex
+++ lib/wwelo_web/schema.ex
@@ -3,11 +3,22 @@ defmodule WweloWeb.Schema do
   use Absinthe.Schema
 
   alias Wwelo.Repo
+  alias Wwelo.Stats
   alias Wwelo.Stats.Wrestler
 
   object :wrestler do
     field(:id, :integer)
     field(:name, :string)
+  end
+
+  object :wrestler_elos do
+    field(:name, :string)
+    field(:current_elo, :elo)
+  end
+
+  object :elo do
+    field(:elo, :float)
+    field(:date, :string)
   end
 
   query do
@@ -18,6 +29,15 @@ defmodule WweloWeb.Schema do
         %{name: wrestler_name} = Repo.get(Wrestler, wrestler_id)
 
         {:ok, %{id: wrestler_id, name: wrestler_name}}
+      end)
+    end
+
+    field :wrestler_elos, :wrestler_elos do
+      resolve(fn _, _ ->
+        %{name: wrestler_name, current_elo: current_elo} =
+          Stats.list_wrestlers_elos() |> Enum.at(0)
+
+        {:ok, %{name: wrestler_name, current_elo: current_elo}}
       end)
     end
   end
