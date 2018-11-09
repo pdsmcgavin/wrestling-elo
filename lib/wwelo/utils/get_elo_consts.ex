bdb0e00f3761dbc1d877934baa89f84a0b9a317a
McGavInvincible <pdsmcgavin@gmail.com>
2018-11-09 17:30:27 +0000

    Forgot about difference between dev and prod for consts file

--- lib/wwelo/utils/get_elo_consts.ex
+++ lib/wwelo/utils/get_elo_consts.ex
@@ -2,8 +2,16 @@ defmodule Wwelo.Utils.GetEloConsts do
   @moduledoc false
 
   def get_elo_consts do
-    with {:ok, body} <- File.read("assets/static/elo-consts.json"),
+    with {:ok, body} <- File.read(elo_consts_location()),
          {:ok, json} <- body |> Poison.Parser.parse(%{keys: :atoms}),
          do: json
+  end
+
+  defp elo_consts_location do
+    if Application.get_env(:wwelo, :environment) == :prod do
+      "priv/static/elo_consts.json"
+    else
+      "assets/static/elo-consts.json"
+    end
   end
 end
