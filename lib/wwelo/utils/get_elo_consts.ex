06c263d8fa573ea3ef01ede586a3aec4f89bd32e
McGavInvincible <pdsmcgavin@gmail.com>
2018-11-12 16:27:08 +0000

    Brought back elo consts to the back end

--- lib/wwelo/utils/get_elo_consts.ex
+++ lib/wwelo/utils/get_elo_consts.ex
@@ -2,16 +2,8 @@ defmodule Wwelo.Utils.GetEloConsts do
   @moduledoc false
 
   def get_elo_consts do
-    with {:ok, body} <- File.read(elo_consts_location()),
+    with {:ok, body} <- File.read("lib/wwelo/utils/elo_consts.json"),
          {:ok, json} <- body |> Poison.Parser.parse(%{keys: :atoms}),
          do: json
-  end
-
-  defp elo_consts_location do
-    if Application.get_env(:wwelo, :environment) == :prod do
-      "priv/static/elo-consts.json"
-    else
-      "assets/static/elo-consts.json"
-    end
   end
 end
