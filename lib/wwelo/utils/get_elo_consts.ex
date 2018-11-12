434b7c04d818260772cd0ef15df8c1c03e4ac9ea
McGavInvincible <pdsmcgavin@gmail.com>
2018-11-12 16:52:01 +0000

    Brought back elo consts to a .ex file

--- lib/wwelo/utils/get_elo_consts.ex
+++ lib/wwelo/utils/get_elo_consts.ex
@@ -2,8 +2,11 @@ defmodule Wwelo.Utils.GetEloConsts do
   @moduledoc false
 
   def get_elo_consts do
-    with {:ok, body} <- File.read("lib/wwelo/utils/elo_consts.json"),
-         {:ok, json} <- body |> Poison.Parser.parse(%{keys: :atoms}),
-         do: json
+    %{
+      default_elo: 1200,
+      match_weight: 32,
+      elo_base: 10,
+      distribution_factor: 400
+    }
   end
 end
