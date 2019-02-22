15d6d25c0f4b9a26e5b01163827875ca38954630
Philip McGavin <pdsmcgavin@gmail.com>
2019-02-22 17:14:37 +0000

    Caching common queries after update

--- lib/wwelo/updater.ex
+++ lib/wwelo/updater.ex
@@ -10,6 +10,7 @@ defmodule Wwelo.Updater do
   alias Wwelo.SiteScraper.Rosters
   alias Wwelo.SiteScraper.Scraper
   alias Wwelo.SiteScraper.TitleHolders
+  alias Wwelo.StatsCache
 
   def start_link(state) do
     Logger.warn("Updater initialised")
@@ -28,6 +29,10 @@ defmodule Wwelo.Updater do
     {:noreply, state}
   end
 
+  defp schedule_work() do
+    Process.send_after(self(), :work, 24 * 60 * 60 * 1000)
+  end
+
   defp update_site do
     Logger.warn("Updating site")
     Scraper.scrape_site()
@@ -36,9 +41,36 @@ defmodule Wwelo.Updater do
     TitleHolders.save_current_title_holders_to_database()
     Logger.warn("Updating site complete")
     Cachex.clear(:wwelo_cache)
+    cache_queries()
   end
 
-  defp schedule_work() do
-    Process.send_after(self(), :work, 24 * 60 * 60 * 1000)
+  def cache_queries do
+    StatsCache.get_elo_stats_by_year()
+    StatsCache.get_title_holders()
+    StatsCache.list_wrestlers_stats(50)
+
+    StatsCache.list_current_wrestlers_stats(
+      10,
+      365,
+      Date.utc_today() |> Date.to_string()
+    )
+
+    StatsCache.list_current_wrestlers_stats(
+      10,
+      365,
+      Date.utc_today() |> Date.add(1) |> Date.to_string()
+    )
+
+    StatsCache.list_current_wrestlers_stats(
+      10,
+      365,
+      Date.utc_today() |> Date.add(-7) |> Date.to_string()
+    )
+
+    StatsCache.list_current_wrestlers_stats(
+      10,
+      365,
+      Date.utc_today() |> Date.add(6) |> Date.to_string()
+    )
   end
 end
