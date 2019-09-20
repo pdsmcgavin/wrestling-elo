6cf4daf583377e6e0c8c175f2cdb9f916a91a41a
Philip McGavin <pdsmcgavin@gmail.com>
2019-09-20 17:09:04 +0100

    Poor encoding

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Wwelo.Mixfile do
   def project do
     [
       app: :wwelo,
-      version: "0.4.14",
+      version: "0.4.15",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
